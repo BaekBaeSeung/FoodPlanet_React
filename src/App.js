ab2a91e2e87fc44ae4a7d092ef085ab138b8283a
BaekBaeSeung <bbs4861@naver.com>
2024-04-29 17:18:57 +0900

    이미지 경로 바꿧으니까 다시 재설정 하세요

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Body from './BBS/2_body/body';
 import Footer from './BBS/3_footer/footer';
-import Card from './BBS/4_card_Test/AnimatedFlexCards ';
+import Card from './BBS/4_card_Test/AnimatedFlexCards';
 import { Link } from 'react-router-dom';
 
 
--- src/BBS/4_card_Test/AnimatedFlexCards.jsx
+++ src/BBS/4_card_Test/AnimatedFlexCards.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from './AnimatedFlexCards.module.css';
-import image1 from "../images/촛불1978_03.PNG";
-import image2 from "../images/촛불1978_04.PNG";
-import image3 from "../images/촛불1978_05.PNG";
-import image4 from "../images/촛불1978_06.PNG";
+import image1 from "./../../images/assets/촛불1978_03.png";
+import image2 from "./../../images/assets/촛불1978_04.png";
+import image3 from "./../../images/assets/촛불1978_05.png";
+import image4 from "./../../images/assets/촛불1978_06.png";
+
 import { useState } from "react";
 
 const AnimatedFlexCards = () => {
@@ -17,7 +18,7 @@ const AnimatedFlexCards = () => {
             <div className={styles.wrapper}>
                 <div className={styles.container}>
                     <input className={styles.input} type="radio" name="slide" id="c1" checked={currentSlide === 1} />
-                    <label htmlFor="c1" className={styles.card} style={{ backgroundImage: `url(${image1})` }} onClick={nextSlide}>
+                    <label htmlFor="c1" className={styles.card} style={{ backgroundImage: `url(${image1})`}} onClick={nextSlide}>
                         <div className={styles.row}>
                             <div className={styles.icon}>1</div>
                             <div className={styles.description}>
@@ -27,7 +28,7 @@ const AnimatedFlexCards = () => {
                         </div>
                     </label>
                     <input className={styles.input} type="radio" name="slide" id="c2" checked={currentSlide === 2} />
-                    <label htmlFor="c2" className={styles.card} style={{ backgroundImage: `url(${image2})` }} onClick={nextSlide}>
+                    <label htmlFor="c2" className={styles.card} style={{ backgroundImage: `url(${image2})`}} onClick={nextSlide}>
                         <div className={styles.row}>
                             <div className={styles.icon}>2</div>
                             <div className={styles.description}>
@@ -37,7 +38,7 @@ const AnimatedFlexCards = () => {
                         </div>
                     </label>
                     <input className={styles.input} type="radio" name="slide" id="c3" checked={currentSlide === 3} />
-                    <label htmlFor="c3" className={styles.card} style={{ backgroundImage: `url(${image3})` }} onClick={nextSlide}>
+                    <label htmlFor="c3" className={styles.card} style={{ backgroundImage: `url(${image3})`}} onClick={nextSlide}>
                         <div className={styles.row}>
                             <div className={styles.icon}>3</div>
                             <div className={styles.description}>
@@ -47,7 +48,7 @@ const AnimatedFlexCards = () => {
                         </div>
                     </label>
                     <input className={styles.input} type="radio" name="slide" id="c4" checked={currentSlide === 4} />
-                    <label htmlFor="c4" className={styles.card} style={{ backgroundImage: `url(${image4})` }} onClick={nextSlide}>
+                    <label htmlFor="c4" className={styles.card} style={{ backgroundImage: `url(${image4})`}} onClick={nextSlide}>
                         <div className={styles.row}>
                             <div className={styles.icon}>4</div>
                             <div className={styles.description}>
