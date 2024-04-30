766b09833f2b7ac0e100d5e7a49662c32b8798fe
LeeEunDeok <t01092561402@gmail.com>
2024-04-30 21:34:18 +0900

    쿠키 진행중

--- src/deok/Routing.jsx
+++ src/deok/Routing.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Account from './Components/Account/Login_Signup'
-import App from './../App'
+import App from './App'
 
 function Routing() {
     return (
