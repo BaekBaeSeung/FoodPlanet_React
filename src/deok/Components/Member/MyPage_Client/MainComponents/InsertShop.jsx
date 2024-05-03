00a292c8929621822bb8fe69b03fa795546fcf88
LeeEunDeok <t01092561402@gmail.com>
2024-05-03 21:40:37 +0900

    24/05/03

---
+++ src/deok/Components/Member/MyPage_Client/MainComponents/InsertShop.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './InsertShop.css'
+
+const InsertShop = () => {
+    return (
+        <>
+            <h1>업체 등록</h1>
+            <div className="DEOK_MP_CL_insertShop">
+
+            </div>
+        </>
+    )
+}
+
+export default InsertShop;
