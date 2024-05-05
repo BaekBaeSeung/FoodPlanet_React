98863bc25a52a0547aa1fc9c75e8922d7c51fe7d
BaekBaeSeung <bbs4861@naver.com>
2024-05-05 18:47:30 +0900

    메인페이지 수정 및 디테일 작업중

--- src/Routing.jsx
+++ src/Routing.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Login from "./BBS/Components/Account/Login_Signup";
-import Login from './deok/Components/Member/Login_Signup/Login_Signup'
-import MpClient from './deok/Components/Member/MyPage_Client/Mypage_Client'
+import Login from "./deok/Components/Member/Login_Signup/Login_Signup";
+import MpClient from "./deok/Components/Member/MyPage_Client/Mypage_Client";
 import App from "./App";
+import ShopDetail from "./BBS/Components/ShopDetailPage/ShopDetail";
 
 function Routing() {
   return (
@@ -12,6 +13,7 @@ function Routing() {
         <Route path="/" element={<App />} />
         <Route path="login" element={<Login />} />
         <Route path="mypage" element={<MpClient />} />
+        <Route path="shopDetail" element={<ShopDetail />} />
       </Routes>
     </BrowserRouter>
   );
